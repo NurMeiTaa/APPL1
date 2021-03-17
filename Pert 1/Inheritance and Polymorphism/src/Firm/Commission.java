375698ef90943c2b72eb4c84bee7ec2dfb66cbcd
NurMeiTaa <61399972+NurMeiTaa@users.noreply.github.com>
2021-03-17 23:34:04 +0700

    SdK 1
    Menyelesaikan SdK 1

--- Pert 1/Inheritance and Polymorphism/src/Firm/Commission.java
+++ Pert 1/Inheritance and Polymorphism/src/Firm/Commission.java
@@ -9,6 +9,28 @@ package Firm;
  *
  * @author dell
  */
-public class Commission {
-    
+
+public class Commission extends Hourly{
+    private double totalSales;
+    private final double commissionRate;
+    public Commission(String eName, String eAddress, String ePhone, String socSecNumber, double rate, double commissionRate) {
+        super(eName, eAddress, ePhone, socSecNumber, commissionRate);
+        this.commissionRate = commissionRate;
+        totalSales = 0;
+    }
+    public void addSales(double totalSales){
+        this.totalSales = this.totalSales + totalSales;
+    }
+    @Override
+    public double pay(){
+        double payment = super.pay() + (totalSales * commissionRate);
+        totalSales = 0;
+        return payment;
+    }
+    @Override
+    public String toString(){
+        String result = super.toString();
+        result += "\nTotal Sales: " + totalSales;
+        return result;
+    }
 }
--- Pert 1/Inheritance and Polymorphism/src/Firm/Staff.java
+++ Pert 1/Inheritance and Polymorphism/src/Firm/Staff.java
@@ -23,7 +23,7 @@ public class Staff
     //-----------------------------------------------------------------
     public Staff ()
     {
-        staffList = new StaffMember[6];
+        staffList = new StaffMember[8];
         staffList[0] = new Executive ("Sam", "123 Main Line",
         "555-0469", "123-45-6789", 2423.07);
         staffList[1] = new Employee ("Carla", "456 Off Line",
@@ -36,8 +36,17 @@ public class Staff
         "555-8374");
         staffList[5] = new Volunteer ("Cliff", "321 Duds Lane",
         "555-7282");
+        
         ((Executive)staffList[0]).awardBonus (500.00);
         ((Hourly)staffList[3]).addHours (40);
+        
+        staffList[6] = new Commission ("Shinta", "11 Mobile ML", "555-3151", "987-65-1234", 6.25, 0.20);
+        staffList[7] = new Commission ("Nurkaafi", "18 Mine Craft", "555-3146", "987-65-1243", 9.75, 0.15);
+        
+        ((Commission)staffList[6]).addHours(35);
+        ((Commission)staffList[6]).addSales(400);
+        ((Commission)staffList[7]).addHours(40);
+        ((Commission)staffList[7]).addSales(950);
     }
     
     //-----------------------------------------------------------------
