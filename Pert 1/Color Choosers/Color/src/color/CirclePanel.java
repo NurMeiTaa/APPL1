d2dcaffc47be2dfe9bf885c71eb4b942381f694f
NurMeiTaa <61399972+NurMeiTaa@users.noreply.github.com>
2021-03-19 23:08:52 +0700

    SdK 6

--- Pert 1/Color Choosers/Color/src/color/CirclePanel.java
+++ Pert 1/Color Choosers/Color/src/color/CirclePanel.java
@@ -7,96 +7,131 @@ package color;
 
 /**
  *
- * @author dell
+ * @author Nisa Shadrina
  */
+// ****************************************************************** 
+// CirclePanel.java 
+// 
+// A panel with a circle drawn in the center and buttons on the 
+// bottom that move the circle. 
+// ****************************************************************** 
+import java.awt.*; 
+import javax.swing.*; 
+import java.awt.event.*; 
+public class CirclePanel extends JPanel{
+    private final int CIRCLE_SIZE = 50; 
+    private int x,y; 
+    private Color c; 
+    private JButton chooseColor = new JButton("Choose Color");
+     //--------------------------------------------------------------- 
+     // Set up circle and buttons to move it. 
+     //--------------------------------------------------------------- 
+    public CirclePanel(int width, int height){
+        // Set coordinates so circle starts in middle 
+        x = (width/2)-(CIRCLE_SIZE/2); 
+        y = (height/2)-(CIRCLE_SIZE/2); 
+        c = Color.green; 
+        // Need a border layout to get the buttons on the bottom 
+        this.setLayout(new BorderLayout()); 
+        // Create buttons to move the circle 
+        JButton left = new JButton("Left"); 
+        JButton right = new JButton("Right"); 
+        JButton up = new JButton("Up"); 
+        JButton down = new JButton("Down"); 
+        // Add listeners to the buttons 
+        left.addActionListener(new MoveListener(-20,0)); 
+        right.addActionListener(new MoveListener(20,0)); 
+        up.addActionListener(new MoveListener(0,-20)); 
+        down.addActionListener(new MoveListener(0,20)); 
+        // Need a panel to put the buttons on or they'll be on 
+        // top of each other. 
+        JPanel buttonPanel = new JPanel(); 
+        buttonPanel.add(left); 
+        buttonPanel.add(right); 
+        buttonPanel.add(up); 
+        buttonPanel.add(down); 
+         // Add the button panel to the bottom of the main panel 
+        this.add(buttonPanel, "South"); 
+        
+        //Create buttons to change the circle's color
+        JButton red = new JButton("Red"); 
+        JButton yellow = new JButton("Yellow");
+        JButton green = new JButton("Green"); 
+        JButton blue = new JButton("Blue");
+        
+        //Add listeners to the buttons 
+        red.addActionListener(new ColorListener(Color.RED));
+        yellow.addActionListener(new ColorListener(Color.YELLOW));
+        chooseColor.addActionListener(new ColorListener(null));
+        green.addActionListener(new ColorListener(Color.GREEN));
+        blue.addActionListener(new ColorListener(Color.BLUE));
+        
+        //Add panel to put the buttons
+        JPanel colorButtonPanel = new JPanel(); 
+        colorButtonPanel.add(red); 
+        colorButtonPanel.add(yellow);
+        colorButtonPanel.add(chooseColor);
+        colorButtonPanel.add(green); 
+        colorButtonPanel.add(blue); 
+        
+        //Add the color panel to the north part of the main panel. 
+        this.add(colorButtonPanel, "North"); 
+        
+        //Add background
+        red.setBackground(Color.RED);
+        yellow.setBackground(Color.YELLOW);
+        chooseColor.setBackground(Color.PINK);
+        green.setBackground(Color.GREEN);
+        blue.setBackground(Color.BLUE);
+    } 
+     //--------------------------------------------------------------- 
+     // Draw circle on CirclePanel 
+     //--------------------------------------------------------------- 
+    @Override
+    public void paintComponent(Graphics page){
+        super.paintComponent(page); 
+        page.setColor(c); 
+        page.fillOval(x,y,CIRCLE_SIZE,CIRCLE_SIZE); 
+    } 
+     //--------------------------------------------------------------- 
+     // Class to listen for button clicks that move circle. 
+     //--------------------------------------------------------------- 
+    private class MoveListener implements ActionListener{
+        private int dx; 
+        private int dy; 
+         //--------------------------------------------------------------- 
+         // Parameters tell how to move circle at click. 
+         //--------------------------------------------------------------- 
+        public MoveListener(int dx, int dy){
+            this.dx = dx; 
+            this.dy = dy; 
+        } 
+         //--------------------------------------------------------------- 
+         // Change x and y coordinates and repaint. 
+         //--------------------------------------------------------------- 
+        @Override
+        public void actionPerformed(ActionEvent e){
+            x += dx; 
+            y += dy; 
+            repaint(); 
+        } 
+    }
+    
+    private class ColorListener implements ActionListener{
+        private Color color;
+        //Parameter give a color to the circle at click
+        public ColorListener(Color newColor){
+            this.color = newColor; 
+        }
 
-// ******************************************************************
-// CirclePanel.java
-//
-// A panel with a circle drawn in the center and buttons on the
-// bottom that move the circle.
-// ******************************************************************
-import java.awt.*;
-import javax.swing.*;
-import java.awt.event.*;
-public class CirclePanel extends JPanel
-{
-private final int CIRCLE_SIZE = 50;
-private int x,y;
-private Color c;
-//---------------------------------------------------------------
-// Set up circle and buttons to move it.
-//---------------------------------------------------------------
-public CirclePanel(int width, int height)
-{
-// Set coordinates so circle starts in middle
-x = (width/2)-(CIRCLE_SIZE/2);
-y = (height/2)-(CIRCLE_SIZE/2);
-c = Color.green;
-// Need a border layout to get the buttons on the bottom
-this.setLayout(new BorderLayout());
-// Create buttons to change the circle's color
-JButton red = new JButton("Red");
-JButton yellow = new JButton("Yellow");
-JButton green = new JButton("Green");
-JButton blue = new JButton("Blue");
-// Add listeners to the buttons
-red.addActionListener(new ColorListener(Color.RED));
-yellow.addActionListener(new ColorListener(Color.YELLOW));
-green.addActionListener(new ColorListener(Color.GREEN));
-blue.addActionListener(new ColorListener(Color.BLUE));
-// Add panel to put the buttons
-JPanel colorbuttonPanel = new JPanel();
-colorbuttonPanel.add(red);
-colorbuttonPanel.add(yellow);
-colorbuttonPanel.add(green);
-colorbuttonPanel.add(blue);
-// Add the color panel to the north part of the main panel.
-this.add(colorButtonPanel, "North");
-}
-//Add BG
-red.setBackground(Color.RED);
-yellow.setBackground(Color.YELLOW);
-green.setBackground(Color.GREEN);
-blue.setBackground(Color.BLUE);
-
-private JButton chooseColor = new JButton("Choose Color");
-chooseColor.addActionListener(new ColorListener(null));
-colorButtonPanel.add(chooseColor);
-chooseColor.setBackground(Color.PINK);
-//---------------------------------------------------------------
-// Draw circle on CirclePanel
-//---------------------------------------------------------------
-public void paintComponent(Graphics page)
-{
-
-super.paintComponent(page);
-page.setColor(c);
-page.fillOval(x,y,CIRCLE_SIZE,CIRCLE_SIZE);
-}
-//---------------------------------------------------------------
-// Class to listen for button clicks that move circle.
-//---------------------------------------------------------------
-private class MoveListener implements ActionListener
-{
-private int dx;
-private int dy;
-//---------------------------------------------------------------
-// Parameters tell how to move circle at click.
-//---------------------------------------------------------------
-public MoveListener(int dx, int dy)
-{
-this.dx = dx;
-this.dy = dy;
-}
-//---------------------------------------------------------------
-// Change x and y coordinates and repaint.
-//---------------------------------------------------------------
-public void actionPerformed(ActionEvent e)
-{
-x += dx;
-y += dy;
-repaint();
-}
-}
-}
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(color == null){
+                c = JColorChooser.showDialog(chooseColor, "Choose Color", c);
+            }else{
+                c = color;
+            }
+            repaint();
+        }
+    }
+} 
--- Pert 1/Color Choosers/Color/src/color/MoveCircle.java
+++ Pert 1/Color Choosers/Color/src/color/MoveCircle.java
@@ -7,29 +7,27 @@ package color;
 
 /**
  *
- * @author dell
+ * @author Nisa Shadrina
  */
+// ****************************************************************** 
+// MoveCircle.java 
+// 
+// Uses CirclePanel to display a GUI that lets the user move 
+// a circle by pressing buttons. 
+// ****************************************************************** 
+import java.awt.*; 
+import javax.swing.*; 
 
-// ******************************************************************
-// MoveCircle.java
-//
-// Uses CirclePanel to display a GUI that lets the user move
-// a circle by pressing buttons.
-// ******************************************************************
-import java.awt.*;
-import javax.swing.*;
-public class MoveCircle
-{
-//-----------------------------------
-// Set up a frame for the GUI.
-//-----------------------------------
-public static void main(String[] args)
-{
-JFrame frame = new JFrame ("MoveCircle");
-frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-frame.setPreferredSize(new Dimension(400,300));
-frame.getContentPane().add(new CirclePanel(400,300));
-frame.pack();
-frame.setVisible(true);
-}
-}
+public class MoveCircle{ 
+ //----------------------------------- 
+ // Set up a frame for the GUI. 
+ //----------------------------------- 
+    public static void main(String[] args){ 
+        JFrame frame = new JFrame ("MoveCircle"); 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        frame.setPreferredSize(new Dimension(900,500)); 
+        frame.getContentPane().add(new CirclePanel(900,500)); 
+        frame.pack(); 
+        frame.setVisible(true); 
+    } 
+} 
