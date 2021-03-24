f719dc50ea94feaf376ed70df5c0b6dd11839220
NurMeiTaa <61399972+NurMeiTaa@users.noreply.github.com>
2021-03-25 02:56:05 +0700

    SdK 4

---
+++ Pert 2/File Input and Output/SdK 4/src/sdk4/SdK4.java
@@ -0,0 +1,29 @@
+//Author: Shinta Nurkaafi
+package sdk4;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class SdK4 {
+    public static void main(String[] args) {
+        String filename;
+        do{
+            System.out.print("Name of file is: ");
+            Scanner filenameScan = new Scanner(System.in);
+            filename = filenameScan.nextLine();
+            try{
+                //scan file
+                File file = new File(filename);
+                Scanner fileScan = new Scanner(file);
+
+                //search and print
+                while(fileScan.hasNextLine())
+                    System.out.println(fileScan.nextLine());
+                break;
+            }catch(FileNotFoundException e){
+                System.out.println("File tidak ditemukan!");
+                filename ="";
+            }
+        }while (filename.isEmpty());
+    }
+}
