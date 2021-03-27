76ea880f1b4cf6b2576402c962dbf9b54f405857
NurMeiTaa <61399972+NurMeiTaa@users.noreply.github.com>
2021-03-27 20:20:27 +0700

    SdK 3

--- Pert 2/Exceptions/SdK 3/SdK 3/src/sdk3/SdK3.java
+++ Pert 2/Exceptions/SdK 3/SdK 3/src/sdk3/SdK3.java
@@ -1,21 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Author: Shinta Nurkaafi
 package sdk3;
+import java.util.Scanner;
 
-/**
- *
- * @author dell
- */
 public class SdK3 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String keepGoing = “y”;
+        Scanner scan = new Scanner(System.in);
+        
+        while (keepGoing.equals(“y”) || keepGoing.equals(“Y”)){
+            System.out.print("Enter an integer: ");
+            int val = scan.nextInt();
+            System.out.println("Factorial(" + val + ") = " + MathUtils.factorial(val));
+            System.out.print("Another factorial? (y/n) ");
+            keepGoing = scan.next();
+        }
     }
-    
 }
