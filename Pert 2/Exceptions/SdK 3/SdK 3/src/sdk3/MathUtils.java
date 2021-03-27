f7a0f1c0ce0d37a063a453612ec285b6c60b7e63
NurMeiTaa <61399972+NurMeiTaa@users.noreply.github.com>
2021-03-28 00:50:43 +0700

    SdK 3

--- Pert 2/Exceptions/SdK 3/SdK 3/src/sdk3/MathUtils.java
+++ Pert 2/Exceptions/SdK 3/SdK 3/src/sdk3/MathUtils.java
@@ -2,10 +2,16 @@
 package sdk3;
 
 public class MathUtils{
-    public static int factorial(int n){
-        int fac = 1;
-        for (int i=n; i>0; i--)
-            fac *= i;
-        return fac;
+    public static int factorial(int n) throws IllegalArgumentException{
+        if (n > -1 && n <= 16){
+            int fac = 1;
+            for (int i=n; i>0; i--)
+                fac *= i;
+            return fac;
+        }else if (n <= -1){
+            throw new IllegalArgumentException("Invald Input!");
+        }else {
+            throw new IllegalArgumentException("Sorry, only can count 1 until 16 for the integer");
+        }
     }
 }
--- Pert 2/Exceptions/SdK 3/SdK 3/src/sdk3/SdK3.java
+++ Pert 2/Exceptions/SdK 3/SdK 3/src/sdk3/SdK3.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 
 public class SdK3 {
     public static void main(String[] args) {
-        String keepGoing = “y”;
+        String keepGoing = "Y";
         Scanner scan = new Scanner(System.in);
         
-        while (keepGoing.equals(“y”) || keepGoing.equals(“Y”)){
+        while (keepGoing.equals("y") || keepGoing.equals("Y")){
             System.out.print("Enter an integer: ");
             int val = scan.nextInt();
-            System.out.println("Factorial(" + val + ") = " + MathUtils.factorial(val));
+            try{
+                System.out.println("Factorial(" + val + ") = "+ MathUtils.factorial(val));
+            } catch(IllegalArgumentException exception) {
+                System.out.println(exception + "\n");
+            }
             System.out.print("Another factorial? (y/n) ");
             keepGoing = scan.next();
         }
